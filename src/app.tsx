6f5e940704efb7c6d0817c5c522ca0d8228a6358
Raja Dastidar <rajadastidarking@gmail.com>
2021-08-24 10:44:40 +0530

    last commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import IMAGE from './App.png';
 import LOGO from './logo.svg'
+
+import { Counter } from './Counter'
 const App = (): JSX.Element => {
   return (
     <React.Fragment>
       <h1>React TypeScript Webpack Starter Template</h1>
       <img src={IMAGE} alt="image" width="300" height="200"/>
       <img src={LOGO} alt="image" width="300" height="200"/>
+      <Counter/>
     </React.Fragment>
   )
 }
